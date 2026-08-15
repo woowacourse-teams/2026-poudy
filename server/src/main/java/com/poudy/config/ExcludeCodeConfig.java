99b900d9139deeedd4f71cd72601c7037e16b64b
armd482 <armd482@naver.com>
2026-08-16 00:27:22 +0900

    refactor : 문서가 설명하는 조립 주석을 지운다

--- server/src/main/java/com/poudy/config/ExcludeCodeConfig.java
+++ server/src/main/java/com/poudy/config/ExcludeCodeConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ExcludeCodeConfig {
 
-    // 제외 성분군과 성분의 매핑은 도메인 지식이지만 성분 데이터를 읽어 와야 세울 수 있다.
-    // 도메인이 Repository 를 알지 않도록 조립만 여기서 한다.
     @Bean
     public ExcludeCodeIngredients excludeCodeIngredients(IngredientRepository ingredientRepository) {
         return new ExcludeCodeIngredients(ingredientRepository.findAll());
