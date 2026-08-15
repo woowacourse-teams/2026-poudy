a30d8ba9ca4adf9a06bf1938f65b906d54c45c8b
armd482 <armd482@naver.com>
2026-08-16 00:05:59 +0900

    test : 제약 문구 검증이 실제로 걸린 애노테이션을 읽게 한다
    - 선언부 기본값을 리플렉션으로 뒤지는 대신 ProductFilterRequest 에 붙은 제약을 읽는다
    - 문구를 덮어써도 오류 코드 연결이 끊기면 잡힌다

--- server/src/test/java/com/poudy/product/controller/dto/ConflictingIngredientFilterTest.java
+++ server/src/test/java/com/poudy/product/controller/dto/ConflictingIngredientFilterTest.java
@@ -3,8 +3,6 @@ package com.poudy.product.controller.dto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.poudy.exception.ErrorCode;
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +10,11 @@ import org.junit.jupiter.api.Test;
 class ConflictingIngredientFilterTest {
 
     @Test
-    @DisplayName("기본 문구가 응답에 실을 오류 코드 이름이다")
-    void defaultMessageNamesErrorCode() {
-        assertThat(ErrorCode.from(defaultMessageOf(ConflictingIngredientFilter.class)))
-                .contains(ErrorCode.CONFLICTING_INGREDIENT_FILTER);
-    }
+    @DisplayName("제품 필터 요청에 걸린 문구가 응답에 실을 오류 코드 이름이다")
+    void messageNamesErrorCode() {
+        ConflictingIngredientFilter constraint = ProductFilterRequest.class
+                .getAnnotation(ConflictingIngredientFilter.class);
 
-    private static String defaultMessageOf(Class<? extends Annotation> constraint) {
-        return Arrays.stream(constraint.getDeclaredMethods()).filter(method -> "message".equals(method.getName()))
-                .map(method -> (String) method.getDefaultValue()).findFirst().orElseThrow();
+        assertThat(ErrorCode.from(constraint.message())).contains(ErrorCode.CONFLICTING_INGREDIENT_FILTER);
     }
 }
