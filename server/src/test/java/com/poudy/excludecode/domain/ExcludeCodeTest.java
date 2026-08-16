a3408061528723ad62ac9687911867d24393cff5
armd482 <armd482@naver.com>
2026-08-16 09:52:50 +0900

    test : 빠른 제외 성분군의 구성과 선언 순서를 고정한다
    - 성분군이 승인한 여섯 개인지 열거해 확인한다
    - 조회 응답 테스트의 인덱스 단언이 기대는 선언 순서를 함께 고정한다
    
    Ref: #42

--- server/src/test/java/com/poudy/excludecode/domain/ExcludeCodeTest.java
+++ server/src/test/java/com/poudy/excludecode/domain/ExcludeCodeTest.java
@@ -12,6 +12,17 @@ import org.junit.jupiter.api.Test;
 class ExcludeCodeTest {
 
     @Test
+    void 빠른_제외_성분군은_승인한_여섯_개를_선언_순서대로_가진다() {
+        assertThat(ExcludeCode.values()).containsExactly(
+                ExcludeCode.FRAGRANCE_ALLERGENS,
+                ExcludeCode.DRYING_ALCOHOLS,
+                ExcludeCode.HARSH_PRESERVATIVES,
+                ExcludeCode.SULFATES,
+                ExcludeCode.CYCLIC_SILICONES,
+                ExcludeCode.SYNTHETIC_COLORANTS);
+    }
+
+    @Test
     void 각_제외_성분군은_표시명과_설명을_가진다() {
         assertThat(Arrays.asList(ExcludeCode.values())).allSatisfy(code -> {
             assertThat(code.displayName()).isNotBlank();
