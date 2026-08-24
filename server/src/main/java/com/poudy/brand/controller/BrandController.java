ab5b4030875f9ef0e133f1555a9614ae86901a00
armd482 <armd482@naver.com>
2026-08-24 16:21:39 +0900

    refactor : 브랜드 컨트롤러 응답 변환 과정 분리

--- server/src/main/java/com/poudy/brand/controller/BrandController.java
+++ server/src/main/java/com/poudy/brand/controller/BrandController.java
@@ -2,6 +2,8 @@ package com.poudy.brand.controller;
 
 import com.poudy.brand.controller.dto.BrandDetailResponse;
 import com.poudy.brand.controller.dto.BrandOverviewResponse;
+import com.poudy.brand.domain.BrandCounts;
+import com.poudy.brand.domain.BrandDetail;
 import com.poudy.brand.service.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -26,13 +28,19 @@ public class BrandController {
     @Operation(summary = "브랜드 조회", description = "전체 브랜드를 브랜드명 오름차순으로 조회하고 브랜드마다 제품 수를 함께 싣는다.")
     @GetMapping
     public ResponseEntity<BrandOverviewResponse> findBrands() {
-        return ResponseEntity.ok(BrandOverviewResponse.from(brandService.findBrands()));
+        BrandCounts brandCounts = brandService.findBrands();
+        BrandOverviewResponse response = BrandOverviewResponse.from(brandCounts);
+
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "브랜드 상세 조회", description = "브랜드 ID 에 해당하는 정보와 이 브랜드 제품이 속한 카테고리를 조회한다. "
             + "브랜드에 속한 제품은 제품 조회에서 brandIds 로 받는다.")
     @GetMapping("/{brandId}")
     public ResponseEntity<BrandDetailResponse> findBrand(@Parameter(example = "12") @PathVariable Long brandId) {
-        return ResponseEntity.ok(BrandDetailResponse.from(brandService.findDetail(brandId)));
+        BrandDetail brandDetail = brandService.findDetail(brandId);
+        BrandDetailResponse response = BrandDetailResponse.from(brandDetail);
+
+        return ResponseEntity.ok(response);
     }
 }
