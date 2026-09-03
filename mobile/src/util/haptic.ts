eed95136351bf24c479cb8e5b0f3ff5110f9eb05
armd482 <armd482@naver.com>
2026-09-03 11:56:44 +0900

    fix : 안드로이드 햅틱 미지원 기기 폴백 추가
    - 시스템 터치 피드백 설정과 기본 클릭 효과 지원 여부를 분리해 확인
    - 지원 기기는 Virtual_Key를 유지하고 미지원 기기는 50ms 진동으로 대체
    
    Ref: #374

--- mobile/src/util/haptic.ts
+++ mobile/src/util/haptic.ts
@@ -1,9 +1,14 @@
 import * as Haptics from 'expo-haptics';
+import PoudyHapticsModule from 'poudy-haptics';
 import { Platform } from 'react-native';
 
 export const playSelectionHaptic = () => {
   if (Platform.OS === 'android') {
-    void Haptics.performAndroidHapticsAsync(Haptics.AndroidHaptics.Virtual_Key).catch(() => undefined);
+    const haptic = PoudyHapticsModule
+      ? PoudyHapticsModule.performSelectionAsync()
+      : Haptics.performAndroidHapticsAsync(Haptics.AndroidHaptics.Virtual_Key);
+
+    void haptic.catch(() => undefined);
     return;
   }
 
